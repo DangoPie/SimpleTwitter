44e6fc9c3dde82162cfd897fed28b489d6f87749
DangoPie <Jessiesu3@gmail.com>
2021-09-28 22:44:12 -0400

    Walkthrough 1-4 completed

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,30 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    public String name;
+    public String screenName;
+    public String profileImageUrl;
+
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+        return user;
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i ++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+    }
+}
